6ef59239d0e5c81ea3504ed4c6d332b240456af9
闵静纬 <minjingwei@minjingweideMacBook-Pro.local>
2021-05-02 15:13:09 +0800

    fix table text color problem

--- frontend/src/homePage/index.js
+++ frontend/src/homePage/index.js
@@ -165,19 +165,14 @@ export default function HomePage() {
                 </Menu>
                 */}
                 <Table classes={{ root: classes.table }}>
-                        <TableBody className={styles.tableStyle}>
-                            <TableRow style={{backgroundColor:'#333333'}} className={styles.tableStyle}>
-                                <TableCell className={styles.tableStyle} style={{borderBottom:"none"}} align="centre">Song</TableCell>
-                                <TableCell style={{borderBottom:"none"}} align="centre" rowSpan={2}>
-                                    <img 
-                                        src="https://i.scdn.co/image/ab67616d00004851c34e9b33adb0030233a6efef"
-                                        className={styles.image}
-                                    />
-                                </TableCell>
+                        <TableBody >
+                            <TableRow style={{backgroundColor:'#333333'}} >
+                                <TableCell style={{borderBottom:"none", color:"white"}} align="centre">Song</TableCell>
+                                <TableCell style={{borderBottom:"none", color:"white"}} align="centre" rowSpan={2}><img src="song.png"/></TableCell>
 
                             </TableRow>
                             <TableRow style={{backgroundColor:'#333333'}}>
-                                <TableCell className={styles.tableStyle} style={{borderBottom:"none"}} align="centre">Artist</TableCell>
+                                <TableCell style={{borderBottom:"none", color:"white"}} align="centre">Artist</TableCell>
                                 
 
                             </TableRow>
@@ -185,21 +180,16 @@ export default function HomePage() {
                     </Table>
 
                     <Table classes={{ root: classes.table }}>
-                    <TableBody className={styles.tableStyle}>
+                    <TableBody >
                             <TableRow style={{backgroundColor:'#333333'}}>
-                                <TableCell className={styles.tableStyle} style={{borderBottom:"none"}} align="centre">Song</TableCell>
-                                <TableCell style={{borderBottom:"none"}} align="centre" rowSpan={2}>
-                                    <img 
-                                        src="https://i.scdn.co/image/ab67616d00001e024719d135efa3d7567c0d7faa"
-                                        className={styles.image}
-                                    />
-                                </TableCell>
+                                <TableCell style={{borderBottom:"none", color:"white"}} align="centre">Song</TableCell>
+                                <TableCell style={{borderBottom:"none", color:"white"}} align="centre" rowSpan={2}><img src="song.png"/></TableCell>
 
                             </TableRow>
                         
                         
                             <TableRow style={{backgroundColor:'#333333'}}>
-                                <TableCell className={styles.tableStyle} style={{borderBottom:"none"}} align="centre">Artist</TableCell>
+                                <TableCell style={{borderBottom:"none", color:"white"}} align="centre">Artist</TableCell>
                                 
 
                             </TableRow>
